bfb5d8e887ee4b4e99d689a0fe3f7d682709f9a1
olumuyiwa.saka <sakamuyiwa@gmail.com>
2022-01-01 19:28:05 +0100

    New year. Initial commit.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posty</title>
+    <!--<link rel="stylesheet" href="{{asset('css/app.css')}}">-->
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Dashboard</a></li>
+            <li><a href="" class="p-3">Post</a></li>
+        </ul>
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Alex Garrett-Smith</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Log Out</a></li>
+        </ul>
+    </nav>       
+    
     @yield('content')
 </body>
 </html>
