3adcbd800942dd4d90cb1b718ef9e88edcc2e054
olumuyiwa.saka <sakamuyiwa@gmail.com>
2022-02-03 23:44:47 +0100

    DIff for Humans

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,12 +23,12 @@
         @if($posts->count())
             @foreach ($posts as $post)
              <div class="mb-4">
-                 <a href="" class="font-bold ">{{$post->user->name}}</a><span class="text-sm text-gray-600">{{$post->created_at}}</span>
+                 <a href="" class="font-bold ">{{$post->user->name}}</a><span class="text-sm text-gray-600">{{$post->created_at->diffForHumans()}}</span>
                  <p class="mb-2">{{$post->body}}</p>
             </div>   
             @endforeach
         @else
-            There are no posts.
+            <p>There are no posts.</p>
         @endif
     </div>
 </div>
